46e2a73ec4167930d614136b575d1beb162a66e5
hurheena26k-sudo <hurheena26k@gmail.com>
2026-09-24 22:00:09 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -9,8 +9,12 @@ def load_services():
 def get_service_information(user_request):
     services = load_services()
 
+    request = user_request.lower()
+
     for service in services["services"]:
-        if service["name"].lower() in user_request.lower():
+        service_name = service["name"].lower()
+
+        if service_name in request:
             return service
 
     return None
